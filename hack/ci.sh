ad9eaec46b150d3994fd66fd27a1dde92e16e168
Lars Weber <weber@weber-software.com>
2019-07-11 11:01:10 +0200

    Revert "travis-ci: use buildkit"
    This reverts commit 8a6f47f5433340684ac7b2386cafc33daa0eafe5.

--- hack/ci.sh
+++ hack/ci.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -eux -o pipefail
 
-export DOCKER_BUILDKIT=1
-
 docker build -t diuid .
 
 time docker run --cap-add=SYS_PTRACE --rm diuid docker echo "measuring kernel+daemon start-up time"
