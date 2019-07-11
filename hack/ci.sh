d47765213ef9851f37e3f7829081261d1409bdf2
Lars Weber <weber@weber-software.com>
2019-07-11 11:12:57 +0200

    travis-ci: removed invalid echo command

--- hack/ci.sh
+++ hack/ci.sh
@@ -3,6 +3,4 @@ set -eux -o pipefail
 
 docker build -t diuid .
 
-time docker run --cap-add=SYS_PTRACE --rm diuid docker echo "measuring kernel+daemon start-up time"
-
 time docker run --cap-add=SYS_PTRACE --rm diuid docker run hello-world
