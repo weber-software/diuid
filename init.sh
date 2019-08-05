84466f0491bcf7c2460c6fb863bac855351df436
anki-code <anki-code@users.noreply.github.com>
2019-08-05 09:32:13 +0300

    Default docker permissions (#19)
    * Default docker permissions
    
    It allows users in the `docker` group use the docker
    
    * Added DIUID_DOCKERD_FLAGS support for group
    
    * Refactor

--- init.sh
+++ init.sh
@@ -29,6 +29,9 @@ ssh -f -N -o StrictHostKeyChecking=no \
     -R0.0.0.0:2376:127.0.0.1:2376 \
     10.0.2.2
 
+# Change permissions to docker.sock to allow user access
+chmod 0660 /var/run/docker.sock && chown root:$DIUID_DOCKERD_GROUP /var/run/docker.sock
+
 PATH=/usr/bin:$PATH dockerd --userland-proxy-path=$(which diuid-docker-proxy) -H unix:///var/run/docker.sock $DIUID_DOCKERD_FLAGS
 
 ret=$?
